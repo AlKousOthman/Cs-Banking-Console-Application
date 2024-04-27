80e230647ec7cb89c4e7916bd02d8342d6ea971a
KFH <KFH@LAPTOP-TRN-143>
2024-04-27 22:44:48 +0300

    Add project files.

---
+++ ConsoleApp1/BankAccount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+      
+    class BankAccount
+    {
+        private string name;
+        private int accountNumber;
+        private double balance;
+        private double[] transactions = new double[100];
+        private int transactionIndex = 0;
+
+        public BankAccount(string name, int accountNumber, double initialBalance)
+        {
+            this.name = name;
+            this.accountNumber = accountNumber;
+            this.balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+            RecordTransaction(amount);
+            Console.WriteLine($"Deposit of {amount:C} successful!");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                RecordTransaction(-amount); 
+                Console.WriteLine($"Withdrawal of {amount:C} successful!");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds!");
+            }
+        }
+
+        public void ViewBalance()
+        {
+            Console.WriteLine($"Your current balance is: {balance:C}");
+        }
+
+        public void ListTransactions()
+        {
+            Console.WriteLine("Transaction History:");
+            for (int i = 0; i < transactionIndex; i++)
+            {
+                Console.WriteLine($"Transaction {i + 1}: {transactions[i]:C}");
+            }
+        }
+
+        private void RecordTransaction(double amount)
+        {
+            transactions[transactionIndex] = amount;
+            transactionIndex++;
+        }
+    }
+}
+
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Prompting the user for input
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your initial account number:");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your initial balance amount:");
+            double balance = double.Parse(Console.ReadLine());
+
+            // Displaying the entered information
+            Console.WriteLine("\nAccount Information:");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Account Number: " + accountNumber);
+            Console.WriteLine("Balance: " + balance);
+
+            // Further operations can be performed using these variables
+            */
+            Console.WriteLine("Welcome to the Basic Banking Application 🏦");
+            Console.WriteLine("--------------------------------------");
+
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your account number: ");
+            int accountNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your initial deposit: ");
+            double initialBalance = Convert.ToDouble(Console.ReadLine());
+
+            BankAccount account = new BankAccount(name, accountNumber, initialBalance);
+
+            Console.WriteLine("\nAccount setup successful!\n");
+
+            MainMenu(account);
+        }
+
+
+        static void MainMenu(BankAccount account)
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("Main Menu:");
+                Console.WriteLine("1. Deposit 💵");
+                Console.WriteLine("2. Withdraw 💸");
+                Console.WriteLine("3. View Balance 💰");
+                Console.WriteLine("4. List Transactions 📜");
+                Console.WriteLine("5. Exit 👋");
+                Console.Write("Please select an option: ");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        account.Deposit(EnterAmount());
+                        break;
+                    case 2:
+                        account.Withdraw(EnterAmount());
+                        break;
+                    case 3:
+                        account.ViewBalance();
+                        break;
+                    case 4:
+                        account.ListTransactions();
+                        break;
+                    case 5:
+                        Console.WriteLine("Thank you for using our banking application. Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option! Please try again.");
+                        break;
+                }
+
+            } while (choice != 5);
+        }
+
+        static double EnterAmount()
+        {
+            Console.Write("Enter the amount: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
